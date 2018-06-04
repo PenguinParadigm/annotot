1dc9f89d5822050292fc9c10f74f81e850d65c1b
Chris Beer <cabeer@stanford.edu>
2018-06-04 15:52:49 -0700

    Use the engine root route for annotations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Annotot::Engine.routes.draw do
-  resources :annotations, except: %i[new edit show], defaults: { format: :json } do
+  resources :annotations, path: '/', except: %i[new edit show], defaults: { format: :json } do
     collection do
       get 'lists'
     end
